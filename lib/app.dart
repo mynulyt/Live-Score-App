1d3c41bd3256f7e1e11823a4fad60016fbd7ac44
mynulyt <mynulalam27@gmail.com>
2025-11-01 00:12:32 +0600

    main page

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LiveScoreApp extends StatelessWidget {
+  const LiveScoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
