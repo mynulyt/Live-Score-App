1416fbabede92aca853b0fcf5c1d0eba6955955f
mynulyt <mynulalam27@gmail.com>
2025-11-05 22:08:59 +0600

    crud operation

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:live_score/home_page.dart';
 
 class LiveScoreApp extends StatelessWidget {
   const LiveScoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(home: HomePage());
   }
 }
